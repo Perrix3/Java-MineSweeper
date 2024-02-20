7ac54d522f9a6935c746ed71c2f6b645ddc1ce78
perrix <perrix@klatul.com>
2024-02-20 17:23:47 +0100

    Added "User.java" for later use

---
+++ Minesweeper/User.java
@@ -0,0 +1,44 @@
+package Minesweeper;
+
+public class User {
+
+	private String name;
+	private int attempts;
+	private int losses;
+	private int wins;
+	
+	
+	public User(String name, int attempts, int losses, int wins) {
+		this.name=name;
+		this.attempts=attempts;
+		this.losses=losses;
+		this.wins=wins;
+	}
+	
+    //getters
+    public String getNombre(){
+        return nombre;
+    }
+    public int getAttempts(){
+        return attempts;
+    }
+    public int getLosses(){
+        return losses;
+    }
+    public int getWins(){
+        return wins;
+    }
+
+    //Add attempt
+    public void attempt(){
+        attempts++;
+    }
+    //Add losses
+    public void losses(){
+        losses++;
+    }
+    //Add wins
+    public void wins(){
+        wins++;
+    }
+}
